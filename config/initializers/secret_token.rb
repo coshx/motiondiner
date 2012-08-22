3d596e26ae9dba203e30c2401de11f9408d736da
Calvin Delamere <calvin.delamere@gmail.com>
2012-08-22 11:07:39 -0600

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotionDiner::Application.config.secret_token = '325bb1507f301775844bbf9409d42678375d77716f34360bb59d50f13f7b6f6a3947a5306710f6b02e5bef1ca5b717ed975bb311988cb64fb6197089b4fdfde0'
