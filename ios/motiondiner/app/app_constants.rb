0ee1e3bf8708b19fb05ae2656737f976a8092ceb
David Kapp <davekapp@gmail.com>
2012-08-23 17:24:37 -0500

    put in extremely basic truck interface

--- ios/motiondiner/app/app_constants.rb
+++ ios/motiondiner/app/app_constants.rb
@@ -5,5 +5,9 @@ class AppConstants
       UIColor.colorWithRed(0.894, green: 0.922, blue: 0.969, alpha: 1.0)    
     end
 
+    def url      
+      "http://motion-diner-api.herokuapp.com"      
+    end
+
   end
 end
