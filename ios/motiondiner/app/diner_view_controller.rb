9997518315f23671935d4842b01ad51d72e99c3c
Anthony J Burton <ant@coshx.com>
2012-08-24 14:52:18 -0400

    sets location and adjusts zoom on map

--- ios/motiondiner/app/diner_view_controller.rb
+++ ios/motiondiner/app/diner_view_controller.rb
@@ -10,11 +10,43 @@ class DinerViewController < UIViewController
     @mapView.setShowsUserLocation("YES")
     view.addSubview @mapView
 
-    @locationManager = CLLocationManager.alloc.init
-    @locationManager.delegate = self
-    @locationManager.startUpdatingLocation
+    @searchBox = makeSearchBox
+    view.addSubview @searchBox
+
+    @locationManager = setupLocationManager
 
     return true
+  end
+
+  def makeSearchBox
+    searchBox = UITextField.alloc.initWithFrame([[10,20], [300, 20]])
+    searchBox.text = "search"
+    searchBox.opaque = "NO"
+    searchBox.backgroundColor = UIColor.lightGrayColor.colorWithAlphaComponent(0.5)
+
+    return searchBox
+  end
+
+  def setupLocationManager
+    locationManager = CLLocationManager.alloc.init
+    locationManager.delegate = self
+    locationManager.startUpdatingLocation
+
+    return locationManager
+  end
+
+  def locationManager(manager, didUpdateToLocation: newLocation, fromLocation: oldLocation)
+    eventDate = newLocation.timestamp
+    howRecent = eventDate.timeIntervalSinceNow
+
+    coord = newLocation.coordinate
+    setRegion(coord)
+  end
+
+  def setRegion(coord)
+    @span = MKCoordinateSpan.new(0.02, 0.02)
+    @region = MKCoordinateRegion.new(coord, @span)
+    @mapView.setRegion(@region, animated: "YES")
   end
 
 end
