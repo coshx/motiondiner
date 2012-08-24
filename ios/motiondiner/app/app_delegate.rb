2bd004a3c26e45a481fba8cb3c051f264cdf507f
David Kapp <davidkapp@coshx.com>
2012-08-24 00:23:59 -0500

    add in Urban Airship build stuff

--- ios/motiondiner/app/app_delegate.rb
+++ ios/motiondiner/app/app_delegate.rb
@@ -5,13 +5,38 @@ class AppDelegate
     welcomeViewController = WelcomeViewController.alloc.init
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(welcomeViewController)
     @window.makeKeyAndVisible
-    requestNotificaitonPermissions
+    #requestNotificaitonPermissions
+    # Urban Airship setup - Init Airship launch options
+    takeOffOptions = NSMutableDictionary.alloc.init
+    takeOffOptions.setValue(launchOptions, forKey:UAirshipTakeOffOptionsLaunchOptionsKey)
+    UAirship.takeOff(takeOffOptions)
+    # NSMutableDictionary *takeOffOptions = [[[NSMutableDictionary alloc] init] autorelease];
+    # [takeOffOptions setValue:launchOptions forKey:UAirshipTakeOffOptionsLaunchOptionsKey];
+     
+    # // Create Airship singleton that's used to talk to Urban Airship servers.
+    # // Please populate AirshipConfig.plist with your info from http://go.urbanairship.com
+    # [UAirship takeOff:takeOffOptions];
+
+    # now actually register for notifications    
+    UAPush.shared.registerForRemoteNotificationTypes(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)
+# [[UAPush shared]
+#  registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+#                                      UIRemoteNotificationTypeSound |
+#                                      UIRemoteNotificationTypeAlert)];
     true
+  end
+
+  def applicationWillTerminate(application)
+    UAirship.land
   end
 
   def application(application, didRegisterForRemoteNotificationsWithDeviceToken:deviceToken)
     deviceTokenDescription = deviceToken.description
     p "User registered for remote notifications with device token: #{deviceTokenDescription}"
+
+    # Urban Airship registration - Updates the device token and registers the token with UA
+    UAPush.shared.registerDeviceToken(deviceToken)
+    #[[UAPush shared] registerDeviceToken:deviceToken];
   end
 
   def application(application, didFailToRegisterForRemoteNotificationsWithError:error)
