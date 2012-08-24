ed60e25581e80fd31cd90ad98cd0fa2bfdabef1e
David Kapp <davidkapp@coshx.com>
2012-08-23 22:59:39 -0500

    add debugging alerts

--- ios/motiondiner/app/app_delegate.rb
+++ ios/motiondiner/app/app_delegate.rb
@@ -12,14 +12,17 @@ class AppDelegate
   def application(application, didRegisterForRemoteNotificationsWithDeviceToken:deviceToken)
     deviceTokenDescription = deviceToken.description
     p "User registered for remote notifications with device token: #{deviceTokenDescription}"
+    App.alert("Successfully got permission to send notifications: #{deviceTokenDescription}")
   end
 
   def application(application, didFailToRegisterForRemoteNotificationsWithError:error)
     errorMessage = NSString.alloc.initWithFormat("Error in registering for notifications: %@", error)
     p errorMessage
+    App.alert("Failed to get push notifications: #{errorMessage}")
   end
 
   def requestNotificaitonPermissions
+    App.alert("Going to try and request push notifications...")
     notificationTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert
     app = UIApplication.sharedApplication
     app.registerForRemoteNotificationTypes(notificationTypes)
