d2b55379a2fdc10cdabd95100a69dcb86f1a7ea3
David Kapp <davidkapp@coshx.com>
2012-08-23 22:17:07 -0500

    add in request for push notifications and logging of the results

--- ios/motiondiner/app/app_delegate.rb
+++ ios/motiondiner/app/app_delegate.rb
@@ -5,6 +5,23 @@ class AppDelegate
     welcomeViewController = WelcomeViewController.alloc.init
     @window.rootViewController = UINavigationController.alloc.initWithRootViewController(welcomeViewController)
     @window.makeKeyAndVisible
+    requestNotificaitonPermissions
     true
+  end
+
+  def application(application, didRegisterForRemoteNotificationsWithDeviceToken:deviceToken)
+    deviceTokenDescription = deviceToken.description
+    p "User registered for remote notifications with device token: #{deviceTokenDescription}"
+  end
+
+  def application(application, didFailToRegisterForRemoteNotificationsWithError:error)
+    errorMessage = NSString.alloc.initWithFormat("Error in registering for notifications: %@", error)
+    p errorMessage
+  end
+
+  def requestNotificaitonPermissions
+    notificationTypes = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert
+    app = UIApplication.sharedApplication
+    app.registerForRemoteNotificationTypes(notificationTypes)
   end
 end
